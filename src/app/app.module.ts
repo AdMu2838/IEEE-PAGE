09edbbe1c0ddb97c7d0f7e9ff1e856b21dc62111
Agustin Mamani <agusdmu1999@gmail.com>
2024-03-22 12:03:26 -0500

    Eliminado FontAwesomeModule de app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {SharedModule} from "./shared/shared.module";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import {CarouselComponent} from "./shared/carousel/carousel.component";
 
 
@@ -22,7 +22,7 @@ import {CarouselComponent} from "./shared/carousel/carousel.component";
     BrowserAnimationsModule,
     NgbModule,
     SharedModule,
-    FontAwesomeModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
