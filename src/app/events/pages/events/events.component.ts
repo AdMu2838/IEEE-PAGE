b9e1697a0d176cc841fc961cbaa4f915667c65f4
Agustin Mamani <agusdmu1999@gmail.com>
2024-03-26 14:25:04 -0500

    Añadir carrusel de eventos y calendario de eventos

--- src/app/events/pages/events/events.component.ts
+++ src/app/events/pages/events/events.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent {
-
+  carouselSlides = [
+    {image: "assets/images/eventos/12022024-DesarrolloMovil.png"},
+    {image: "assets/images/eventos/22012023-AWS.png"},
+    {image: "assets/images/eventos/22012024-IBMCloud.png"},
+    {image: "assets/images/eventos/24022024-AnalisisDatos.png"},
+    {image: "assets/images/eventos/25122023-EnvenenamientoParametros.png"}
+  ];
 }
