3da4d039a3a54301d3132f2edac6e27eb2b4d0ec
alexisBltz <almamanima@gmail.com>
2024-03-29 01:06:04 -0500

    modificaciones con el header, falta desactivar al child

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component,HostListener,Input } from '@angular/core';
+import { Component,HostListener,Input,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,20 @@ import { Component,HostListener,Input } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  scrolled= true;
-  @Input() fixedTop: boolean = false;
+  scrolled = true;
+  @Input() headerMovil: boolean = false;
+  fixedTop: boolean = true;
 
   @HostListener('window:scroll', [])
-  onWindowScroll(){
-    this.scrolled = window.scrollY === 0;
+  onWindowScroll() {
+    if (this.headerMovil) {
+      this.scrolled = window.scrollY === 0;
+    }
+    else {
+      this.fixedTop = false;
+    }
   }
+
+  // Esta variable controla si el encabezado se mantiene en la parte superior de la página
+
 }
